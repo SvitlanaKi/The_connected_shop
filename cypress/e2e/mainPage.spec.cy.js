93cd6d6c668cb86a1ea6b1a0f7052db4217526a8
SvitlanaKi <kyslova19942@gmail.com>
2024-07-29 21:03:44 +0300

    open home page

---
+++ cypress/e2e/mainPage.spec.cy.js
@@ -0,0 +1,37 @@
+describe('Main Page Test',()=>{
+    beforeEach(()=>{
+        cy.visit('https://theconnectedshop.com')
+    });
+    //Кроки
+    it('Site is Open and Check url',()=>{
+     cy.url()
+         .should('eq','https://theconnectedshop.com/')
+    });
+   /* it('Check title for home page',()=>{
+        cy.title()
+            .should('eq','&nbsp;The Connected Shop - Smart Locks, Smart Sensors, Smart Home & Office&nbsp;')
+        });*/
+
+    context(`viewport descktop`,() =>{
+        cy.viewport(1920,1080);
+        });
+
+    it('should have the correct header elements', () => {
+        cy.get('#section-header')
+            .should('exist')
+    });
+
+    it(`Header Icon`, () =>{
+        cy.get('.Header__Icon')
+            .should('exist')
+        });
+    it(`Header Logo`, () =>{
+        cy.get(`.Header__LogoImage Header__LogoImage--transparent`)
+            .should(`exist`)
+        });
+    it(`Header menu`, () =>{
+        cy.get(`.HorizontalList HorizontalList--spacingExtraLoose`)
+            .should(`exist`)
+    });
+
+});
